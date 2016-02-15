552e4555bf3027b7d9b258949e216f796b8302f7
Sajeev <sajeev@realforce.ch>
2016-02-15 15:23:29 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+;(function(){
+	$('#datetimepicker').datetimepicker({
+		dayOfWeekStart : 1, 
+        lang:'en',
+        defaultTime:'09:00',
+        format:'d/m/Y H:i',
+        step: 15
+	});
+})();
